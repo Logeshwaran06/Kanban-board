a31cf0e9780892e07ac9e4c5d096ed9992cd5717
Logeshwaran <intern@futurethinknow.com>
2025-08-09 19:10:33 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl text-blue-600 bg-black p-4'>Hello, Tzuyu. i love you dear</h1>
+    </>
+  )
+}
+
+export default App
